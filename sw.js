e47d2e3115e20099132598862e6bb2e8961ffaa7
possn <pedrossnunes@gmail.com>
2026-03-18 00:56:13 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
-const CACHE="mb-v1";
-const urls=["./","index.html"];
-
-self.addEventListener("install",e=>{
- e.waitUntil(caches.open(CACHE).then(c=>c.addAll(urls)));
+const CACHE = 'muscleboost-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
-
-self.addEventListener("fetch",e=>{
- e.respondWith(
-   caches.match(e.request).then(r=>r||fetch(e.request))
- );
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
