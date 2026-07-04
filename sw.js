e64e2232e46e09cd5538cb91a41e3fc2f6d1e4be
possn <pedrossnunes@gmail.com>
2026-07-04 10:02:55 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,5 @@
-const CACHE='muscleboost-v26-military';
-const CORE=['./','./index.html','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())
-  );
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())
-  );
-});
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  const request = event.request;
-  const accept = request.headers.get('accept') || '';
-  const isHTML = request.mode === 'navigate' || accept.includes('text/html');
-  if (isHTML) {
-    event.respondWith(
-      fetch(request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE).then(cache => cache.put('./index.html', copy)).catch(() => {});
-          return response;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-  event.respondWith(
-    caches.match(request).then(hit => hit || fetch(request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(request, copy)).catch(() => {});
-      return response;
-    }))
-  );
-});
+const CACHE='mb-operation66-v28';
+const ASSETS=['./','./index.html','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
